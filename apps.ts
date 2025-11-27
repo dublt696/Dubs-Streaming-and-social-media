f2ce0d638234069c23ac2808166794614a92c034
Mathew brewer <66145103+dublt696@users.noreply.github.com>
2025-11-26 20:37:11 -0800

    Create apps.ts

---
+++ apps.ts
@@ -0,0 +1,34 @@
+import { type Server } from "node:http";
+
+import express, { type Express, type Request, Response, NextFunction } from "express";
+import { registerRoutes } from "./routes";
+
+export function log(message: string, source = "express") {
+  const formattedTime = new Date().toLocaleTimeString("en-US", {
+    hour: "numeric",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true,
+  });
+
+  console.log(`${formattedTime} [${source}] ${message}`);
+}
+
+export const app = express();
+
+declare module 'http' {
+  interface IncomingMessage {
+    rawBody: unknown
+  }
+}
+app.use(express.json({
+  verify: (req, _res, buf) => {
+    req.rawBody = buf;
+  }
+}));
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  const start = Date.now();
+  const path = req.path;
+  let capturedJsonResponse: Record<string, any> | undefined = undefined;
